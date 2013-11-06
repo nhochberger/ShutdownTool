bb63f9c447251d1866637a9dd73e59b12df04745
Nico Hochberger <nico@irrwisch.net>
2013-11-06 15:50:54 +0100

    now using EnhancedLabel

--- src/gui/CountDownLabel.java
+++ src/gui/CountDownLabel.java
@@ -1,5 +1,7 @@
 package gui;
 
+import hochberger.utilities.gui.EnhancedLabel;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.text.DecimalFormat;
@@ -9,31 +11,32 @@ import javax.swing.JLabel;
 import edt.EDT;
 
 public class CountDownLabel {
-    private final JLabel label;
+	private final EnhancedLabel label;
 
-    public CountDownLabel() {
-        super();
-        EDT.only();
-        this.label = new JLabel("00:00:00");
-        build();
-    }
+	public CountDownLabel() {
+		super();
+		EDT.only();
+		this.label = new EnhancedLabel("00:00:00");
+		build();
+	}
 
-    private void build() {
-        this.label.setText("00:00:00");
-        this.label.setForeground(Color.LIGHT_GRAY);
-        Font font = this.label.getFont().deriveFont(50f);
-        this.label.setFont(font);
-    }
+	private void build() {
+		this.label.setText("00:00:00");
+		this.label.setForeground(Color.LIGHT_GRAY);
+		Font font = this.label.getFont().deriveFont(50f);
+		this.label.setFont(font);
+		this.label.setRightShadow(2, 2, Color.DARK_GRAY.brighter());
+	}
 
-    public JLabel getLabel() {
-        return this.label;
-    }
+	public JLabel getLabel() {
+		return this.label;
+	}
 
-    public void setRemainingSeconds(final long totalSeconds) {
-        long hours = totalSeconds / 3600;
-        long minutes = (totalSeconds % 3600) / 60;
-        long seconds = (totalSeconds % 60);
-        DecimalFormat formatter = new DecimalFormat("00");
-        this.label.setText(formatter.format(hours) + ":" + formatter.format(minutes) + ":" + formatter.format(seconds));
-    }
+	public void setRemainingSeconds(final long totalSeconds) {
+		long hours = totalSeconds / 3600;
+		long minutes = (totalSeconds % 3600) / 60;
+		long seconds = (totalSeconds % 60);
+		DecimalFormat formatter = new DecimalFormat("00");
+		this.label.setText(formatter.format(hours) + ":" + formatter.format(minutes) + ":" + formatter.format(seconds));
+	}
 }
